dbd79102402c89b6cb8f121fdd225deb9c9669ea
Kiranmoka <kiran29.moka@gmail.com>
2020-04-13 11:52:23 +0530

    Changes as per comments.

--- python/AMI_EBS_ENCRYPTED/AMI_EBS_ENCRYPTED.py
+++ python/AMI_EBS_ENCRYPTED/AMI_EBS_ENCRYPTED.py
@@ -8,7 +8,6 @@
 # or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
 # the specific language governing permissions and limitations under the License.
-
 '''
 #####################################
 ##           Gherkin               ##
@@ -17,7 +16,7 @@ Rule Name:
   AMI_EBS_ENCRYPTED
 
 Description:
-  Check EBS-backed AMI are encrypted.
+  Check EBS-backed AMI is encrypted.
 
 Rationale:
   Encrypting on EBS ensure that no data is written on disk in clear text.
@@ -33,17 +32,15 @@ Reports on:
 
 Scenarios:
   Scenario: 1
-    Given: Verify the AMI target EBS is Encrypted
-      And: AMI target EBS is encrypted
+    Given: AMI target EBS is encrypted
      Then: Return COMPLIANT
   Scenario: 2
-    Given: Verify the AMI target EBS is not Encrypted
-      And: AMI target EBS is not encrypted
+    Given: AMI target EBS is not encrypted
      Then: Return NON_COMPLIANT
 '''
 
-
 from rdklib import Evaluator, Evaluation, ConfigRule, ComplianceType
+
 DEFAULT_RESOURCE_TYPE = 'AWS::::Account'
 
 class AMI_EBS_ENCRYPTED(ConfigRule):
@@ -52,33 +49,17 @@ class AMI_EBS_ENCRYPTED(ConfigRule):
         ec2_client = client_factory.build_client('ec2')
         evaluations = []
         amis = ec2_client.describe_images(Owners=["self"])
-
         for ami in amis.get('Images'):
             block_devices = ami.get('BlockDeviceMappings')
             for block_device in block_devices:
                 ebs = block_device.get('Ebs')
                 if ebs and ebs['Encrypted']:
-                    evaluations.append(
-                        Evaluation(
-                            ComplianceType.COMPLIANT,
-                            ami.get('ImageId'),
-                            DEFAULT_RESOURCE_TYPE
-                        )
-                    )
+                    evaluations.append(Evaluation(ComplianceType.COMPLIANT, ami.get('ImageId'), DEFAULT_RESOURCE_TYPE))
                 else:
-                    evaluations.append(
-                        Evaluation(
-                            ComplianceType.NON_COMPLIANT,
-                            ami.get('ImageId'),
-                            DEFAULT_RESOURCE_TYPE
-                        )
-                    )
+                    evaluations.append(Evaluation(ComplianceType.NON_COMPLIANT, ami.get('ImageId'), DEFAULT_RESOURCE_TYPE,
+                                                  annotation="AMI target EBS is not encrypted"))
         return evaluations
 
-
-################################
-# DO NOT MODIFY ANYTHING BELOW #
-################################
 def lambda_handler(event, context):
     my_rule = AMI_EBS_ENCRYPTED()
     evaluator = Evaluator(my_rule)
--- python/AMI_EBS_ENCRYPTED/AMI_EBS_ENCRYPTED_test.py
+++ python/AMI_EBS_ENCRYPTED/AMI_EBS_ENCRYPTED_test.py
@@ -14,25 +14,12 @@ from mock import patch, MagicMock
 from rdklib import Evaluation, ComplianceType
 import rdklibtest
 
-#############
-
-##############
-# Parameters #
-##############
-
-# Define the default resource to report to Config Rules
 RESOURCE_TYPE = 'AWS::::Account'
-
-#############
-# Main Code #
-#############
 
 MODULE = __import__('AMI_EBS_ENCRYPTED')
 RULE = MODULE.AMI_EBS_ENCRYPTED()
 
 CLIENT_FACTORY = MagicMock()
-
-#example for mocking S3 API calls
 EC2_CLIENT_MOCK = MagicMock()
 
 def mock_get_client(client_name, *args, **kwargs):
@@ -44,48 +31,39 @@ def mock_get_client(client_name, *args, **kwargs):
 class ComplianceTest(unittest.TestCase):
 
     non_encrypted_ami_ebs = {
-        "Images": [
-            {
-                "ImageId": "ami-0112c52e7ca89ee2e",
-                "BlockDeviceMappings": [
-                    {
-                        "Ebs": {
-                            "Encrypted": False
-                        }
-                    }
-                ]
-            }
-        ]
+        "Images": [{
+            "ImageId": "ami-0112c52e7ca89ee2e",
+            "BlockDeviceMappings": [{
+                "Ebs": {
+                    "Encrypted": False
+                }
+            }]
+        }]
     }
-
     encrypted_ami_ebs = {
-        "Images": [
-            {
-                "ImageId": "ami-08d7261cad6c06507",
-                "BlockDeviceMappings": [
-                    {
-                        "Ebs": {
-                            "Encrypted": True
-                        }
-                    }
-                ]
-            }
-        ]
+        "Images": [{
+            "ImageId": "ami-08d7261cad6c06507",
+            "BlockDeviceMappings": [{
+                "Ebs": {
+                    "Encrypted": True
+                }
+            }]
+        }]
     }
 
-
-    def test_compliant_ami(self):
+    def test_scenario1_amitargetebsencrypted_returnscompliant(self):
         EC2_CLIENT_MOCK.describe_images.return_value = self.encrypted_ami_ebs
         response = RULE.evaluate_periodic("", CLIENT_FACTORY, "")
         resp_expected = [
             Evaluation(ComplianceType.COMPLIANT, "ami-08d7261cad6c06507", RESOURCE_TYPE)
         ]
-        rdklibtest.assert_successful_evaluation(self, response, resp_expected, 1)
+        rdklibtest.assert_successful_evaluation(self, response, resp_expected)
 
-    def test_non_compliant_ami(self):
+    def test_scenario2_amitargetebsnotencrypted_returnsnoncompliant(self):
         EC2_CLIENT_MOCK.describe_images.return_value = self.non_encrypted_ami_ebs
         response = RULE.evaluate_periodic("", CLIENT_FACTORY, "")
         resp_expected = [
-            Evaluation(ComplianceType.NON_COMPLIANT, "ami-0112c52e7ca89ee2e", RESOURCE_TYPE)
+            Evaluation(ComplianceType.NON_COMPLIANT, "ami-0112c52e7ca89ee2e", RESOURCE_TYPE,
+                       annotation="AMI target EBS is not encrypted")
         ]
         rdklibtest.assert_successful_evaluation(self, response, resp_expected, 1)
